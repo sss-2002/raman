d9adebe78bfd65df7f31a63fbcd39573bd3edbbb
sss-2002 <13131958708@163.com>
2025-07-29 17:27:56 +0800

    Update biolight.py

--- biolight.py
+++ biolight.py
@@ -1,5 +1,11 @@
 import streamlit as st
 
+
+
+ # 返回主页按钮
+    if st.button("返回主页"):
+        st.session_state.current_page = 'home'
+        st.experimental_rerun()
 def main():
     st.title("西安电子科技大学生物光学实验室（BIOLIGHT LAB）介绍")
     st.markdown("西安电子科技大学生物光学实验室（BIOLIGHT LAB）成立于2015年9月。")
@@ -8,7 +14,4 @@ def main():
     st.markdown("在科研方面，实验室专注于生物光学技术在医学检测中的应用，致力于开发更加精准、高效的检测方法和设备。")
     st.markdown("同时，实验室也积极开展国际合作与交流，与国内外知名科研机构和企业保持着密切的联系。")
 
-    # 返回主页按钮
-    if st.button("返回主页"):
-        st.session_state.current_page = 'home'
-        st.experimental_rerun()
+   
