6a4fc3ed646e0dfccec726826b14983488a98491
ggggggtttt <161571779+ggggggtttt@users.noreply.github.com>
2025-10-21 17:35:26 -1200

    Update Arrangement.py

--- Arrangement.py
+++ Arrangement.py
@@ -1648,10 +1648,8 @@ def main():
                     st.session_state.algorithm_permutations = generate_permutations(selected_algorithms)
                     st.session_state.filtered_perms = st.session_state.algorithm_permutations
                     st.success(f"✅ 生成{len(st.session_state.algorithm_permutations)}种方案")
-                    processed_results = calculate_processed_spectra_for_all_arrangements(
-                        preprocessor, wavenumbers, st.session_state.raw_data[1], selected_algorithms
-                         )
-            # 存储处理后的结果
+                    processed_results = calculate_processed_spectra_for_all_arrangements(preprocessor, wavenumbers, st.session_state.raw_data[1], selected_algorithms)
+                    # 存储处理后的结果
                     st.session_state.arrangement_details = processed_results
                 else:
                     st.session_state.filtered_perms = []
